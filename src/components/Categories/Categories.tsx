06404defa3f55f3921b7ed255a6e002b1b5be6a2
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-01-18 20:39:13 +0200

    Create components for Tags

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,21 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import dynamic from "next/dynamic";
+import { TagsMapping } from "../PostLabels";
 
 const CategorieWrapperStyle = styled.div`
     padding: 0.5rem 0 2rem;
     text-align: center;
-
     span {
-        background: var(--color-secondary-700);
-        box-shadow: var(--light-shadow);
-        border-radius: 8px;
-        text-align: center;
-        padding: 0.7rem;
-        color: white;
-        font-size: 1.4rem;
-        letter-spacing: 1.4px;
-        font-weight: var(--font-medium);
+        border-radius: 5px;
     }
 `;
 const CategorieStyle = styled.div`
@@ -30,13 +21,14 @@ export const Categories: React.FC<any> = ({ categories, style }) => {
     return (
         <CategorieWrapperStyle style={{ ...style }}>
             {categories
-                .map((category: string) => category.trim())
-                .filter(category => category.length > 0)
-                .map((category: string, index: number) => (
-                    <CategorieStyle key={index}>
-                        <span>{category}</span>
-                    </CategorieStyle>
-                ))}
+                .map((category: string) => category.trim().toLowerCase())
+                .map((category: string) =>
+                    TagsMapping[category] ? (
+                        <CategorieStyle key={category}>{TagsMapping[category]()}</CategorieStyle>
+                    ) : (
+                        ""
+                    )
+                )}
         </CategorieWrapperStyle>
     );
 };
---
+++ src/components/PostLabels.tsx
@@ -0,0 +1,109 @@
+import { FaPython, FaNodeJs } from "react-icons/fa";
+import { SiJavascript, SiTypescript, SiReact, SiNestjs, SiPostgresql, SiDjango } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+import styled from "styled-components";
+
+const TagWrapper = styled.span`
+    border: 1px solid white;
+    padding: 0.7rem 1rem;
+    display: inline-flex;
+    align-items: center;
+    margin: 0.5rem;
+
+    > span {
+        position: relative;
+        right: 5px;
+    }
+`;
+
+export const Python = (size: number = 22) => {
+    return (
+        <TagWrapper style={{ border: "1px solid #f7df1e", background: "#403a0a" }}>
+            <span>Python</span>
+            <FaPython size={size} />
+        </TagWrapper>
+    );
+};
+
+export const Django = (size: number = 22) => {
+    return (
+        <TagWrapper style={{ border: "1px solid #18c490", background: "#093225" }}>
+            <span>Django</span>
+            <SiDjango size={size} style={{ color: "##18c490" }} />
+        </TagWrapper>
+    );
+};
+
+export const NodeJs = (size: number = 22) => {
+    return (
+        <TagWrapper style={{ border: "1px solid #47a248", background: "#153c15" }}>
+            <span>Node.js</span>
+            <FaNodeJs size={size} style={{ color: "#47a248" }} />
+        </TagWrapper>
+    );
+};
+
+export const JavaScript = (size: number = 22) => {
+    return (
+        <TagWrapper style={{ border: "1px solid #f7df1e", background: "#403a0a" }}>
+            <span>JavaScript</span>
+            <SiJavascript size={size} style={{ color: "#f7df1e" }} />
+        </TagWrapper>
+    );
+};
+
+export const TypeScript = (size: number = 22) => {
+    return (
+        <TagWrapper style={{ background: "#0e344e", border: "1px solid #007acc" }}>
+            <span>TypeScript</span>
+            <SiTypescript size={size} style={{ color: "#007acc" }} />
+        </TagWrapper>
+    );
+};
+
+export const React = (size: number = 22) => {
+    return (
+        <TagWrapper style={{ background: "#1c3e47", border: "1px solid #63d9fa" }}>
+            <span>React</span>
+            <SiReact size={size} style={{ color: "#63d9fa" }} />
+        </TagWrapper>
+    );
+};
+
+export const NextJs = (size: number = 22) => {
+    return (
+        <TagWrapper>
+            <span>Next.js</span> <TbBrandNextjs size={size} />
+        </TagWrapper>
+    );
+};
+
+export const NestJs = (size: number = 22) => {
+    return (
+        <TagWrapper style={{ background: "#440a17", border: "1px solid #e1234d" }}>
+            <span>Nest.js</span>
+            <SiNestjs size={size} style={{ color: "#e1234d" }} />
+        </TagWrapper>
+    );
+};
+
+export const PostgreSQL = (size: number = 22) => {
+    return (
+        <TagWrapper style={{ background: "#0e344e", border: "1px solid #15bbe8" }}>
+            <span>PostgreSQL</span>
+            <SiPostgresql size={size} style={{ color: "##15bbe8" }} />
+        </TagWrapper>
+    );
+};
+
+export const TagsMapping = {
+    python: Python,
+    django: Django,
+    typescript: TypeScript,
+    postgresql: PostgreSQL,
+    nextjs: NextJs,
+    nestjs: NestJs,
+    react: React,
+    nodejs: NodeJs,
+    javascript: JavaScript
+};
