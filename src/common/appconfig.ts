4c6385b9b12e970e307156a3d96a0bf6eff5d2d1
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-01-29 20:07:22 +0200

    Update URL for logo rss feed

--- src/common/appconfig.ts
+++ src/common/appconfig.ts
@@ -2,7 +2,7 @@ export const config = {
     title: "Viktor Sokolov - Developer, creator, writer",
     siteUrl: "https://viktorsokolov.com",
     lang: "en",
-    siteLogo: "../../public/static/logo.svg",
+    siteLogo: `https://viktorsokolov.com/static/logo.svg`,
     siteName: "victory-sokolov",
     description: "Blog about Software Enginnering, TypeScript, Python, Node.js, React",
     author: "Viktor Sokolov",
