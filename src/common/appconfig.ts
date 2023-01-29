c45218b3a3e70d4208e9a693dd79d6de808396a6
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-01-29 11:54:43 +0200

    update domain and description

--- src/common/appconfig.ts
+++ src/common/appconfig.ts
@@ -1,11 +1,10 @@
 export const config = {
     title: "Viktor Sokolov - Developer, creator, writer",
-    // siteUrl: "https://victorysokolov.dev",
-    siteUrl: "http://localhost:3000",
+    siteUrl: "https://viktorsokolov.com",
     lang: "en",
     siteLogo: "static/logo.png",
     siteName: "victory-sokolov",
-    description: "Blog about Software Enginnering",
+    description: "Blog about Software Enginnering, TypeScript, Python, Node.js, React",
     author: "Viktor Sokolov",
     email: process.env.EMAIL_ADDRESS,
     keywords: ["Software Engineering", "Sass business", "Web development", "Python", "JavaScript"],
