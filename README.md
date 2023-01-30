47de30364478cc9a7ead2fea9a8694a5f6c61d2f
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-01-30 19:32:52 +0200

    Fetch latest posts and tips and add them to README file

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
-# viktorsokolov.com
+# [viktorsokolov.com](https://www.viktorsokolov.com)
 
 My blog where I share things related to software development, and tips from my <a href="https://twitter.com/victorysokolov">Twitter</a>
 
 
 ## Tech Stack
 
-* **Framework**: Next.js with TypeScript
+* **Framework**: Next.js, TypeScript
 * **Deployment**: Vercel
 * **Styling**: Styled Components
 
 ![Viktor Sokolov Blog](https://i.imgur.com/46BTIiF.png)
+
+
+## ✍️ Recent blog posts
+
+<!-- POST-START -->
+- [Introduction to Regular Expressions](https://viktorsokolov.com/blog/introduction-to-regular-expressions)
+- [SOLID Principles in Action: Real-World Examples Using Typescript](https://viktorsokolov.com/blog/solid-principles-in-action)
+- [How to implement  Dependency Injection in Python](https://viktorsokolov.com/blog/how-to-implement-dependency-injection-in-python)
+<!-- POST-END -->
+
+## 🔥 Recent tips
+
+<!-- TIP-START -->
+- [Delete all your node_modules, venv and vendor folders periodically](https://viktorsokolov.com/tip/delete-all-your-node_modules-venv-and-vendor-folders-periodically)
+- [Auto reload Celery on any code changes](https://viktorsokolov.com/tip/auto-reload-celery-on-any-code-changes)
+<!-- TIP-END -->
---
+++ lib/latest-posts.ts
@@ -0,0 +1,64 @@
+import fs from "fs";
+import axios from "axios";
+import parser from "xml2json";
+import { config } from "../src/common/appconfig";
+
+const FEED_URL = `${config.siteUrl}/rss.xml`;
+const TEMPLATE = [
+    {
+        open: "<!-- POST-START -->",
+        close: "<!-- POST-END -->"
+    },
+    {
+        open: "<!-- TIP-START -->",
+        close: "<!-- TIP-END -->"
+    }
+];
+const LATEST_N_POSTS = 5;
+
+const toMarkDownLinks = (links: []) => {
+    return links
+        .slice(0, LATEST_N_POSTS)
+        .map(({ title, link }) => `- [${title}](${link})`)
+        .join("\n");
+};
+
+const fetchLatestContent = async () => {
+    const articles = await axios.get(FEED_URL);
+    const articlesText = await articles.data;
+    const articlesJSON = parser.toJson(articlesText);
+    const posts = JSON.parse(articlesJSON).rss.channel.item.filter(item => !item.link.includes("/tip/"));
+    const tips = JSON.parse(articlesJSON).rss.channel.item.filter(item => item.link.includes("/tip/"));
+
+    return {
+        post: toMarkDownLinks(posts),
+        tip: toMarkDownLinks(tips)
+    };
+};
+
+async function main() {
+    const content = await fetchLatestContent();
+
+    TEMPLATE.forEach(tag => {
+        const readme = fs.readFileSync("./README.md", "utf8");
+
+        const indexBefore = readme.indexOf(tag.open) + tag.open.length;
+        const indexAfter = readme.indexOf(tag.close);
+        const readmeContentChunkBreakBefore = readme.substring(0, indexBefore);
+        const readmeContentChunkBreakAfter = readme.substring(indexAfter);
+        const contentType = tag.open.split("-").at(2).toLowerCase().trim();
+        const latestContent = `
+${readmeContentChunkBreakBefore}
+${content[contentType]}
+${readmeContentChunkBreakAfter}
+    `;
+
+        fs.writeFileSync("./README.md", latestContent.trim());
+    });
+}
+
+try {
+    main();
+} catch (error) {
+    console.error(error);
+}
