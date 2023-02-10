4b75c5b330c0827b5368438f4b2a291d55352775
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-02-10 16:41:16 +0200

    Fix site name in Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 import { FooterStyles } from "./Footer.styled";
 import SocialMedia from "@components/Social";
+import { config } from "@common/appconfig";
 
 export const Footer: React.FC = () => {
     return (
         <FooterStyles>
             <div className="footer-content">
                 <Link href="/" aria-label="Home">
-                    VictorySokolov.dev
+                    {config.siteName}.com
                 </Link>
                 <SocialMedia />
                 <p>&copy; {new Date().getFullYear()}. Developed by Viktor Sokolov</p>
