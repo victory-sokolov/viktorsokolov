2c9ee75b59887a297e6463a1ef089fcb0bc55918
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-02-11 19:55:09 +0200

    Add security headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,49 @@
 /** @type {import('next').NextConfig} */
 
+const ContentSecurityPolicy = `
+    default-src 'self';
+    img-src * self 'unsafe-inline' blob: data:;
+    script-src 'unsafe-inline' 'unsafe-eval';
+    script-src-elem 'self' 'unsafe-inline' https://cdn.vercel-insights.com/v1/script.debug.js https://utteranc.es/client.js;
+    style-src 'unsafe-inline';
+    frame-src *.substack.com https://utteranc.es/;
+    font-src 'self' data: https://fonts.googleapis.com;
+`;
+
 const config = {
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    // {
+                    //     key: "Content-Security-Policy",
+                    //     value: "default-src 'unsafe-inline'; image-src 'self'; font-src 'self' 'https://fonts.googleapis.com'; script-src 'unsafe-inline'; frame-src *.substack.com"
+                    // },
+                    {
+                        key: "Content-Security-Policy",
+                        value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim()
+                    },
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY"
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff"
+                    },
+                    {
+                        key: "Referrer-Policy",
+                        value: "origin-when-cross-origin"
+                    },
+                    {
+                        key: "Strict-Transport-Security",
+                        value: "max-age=63072000; includeSubDomains; preload"
+                    }
+                ]
+            }
+        ];
+    },
     reactStrictMode: true,
     swcMinify: true,
     experimental: { esmExternals: true },
