2f226a620db2c6bee112fa187e9aef6ec972d28f
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-05-06 21:31:36 +0300

    Remove strategy from script tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,7 +31,7 @@ export default class MyDocument extends Document {
                             __html: blockingSetInitialColorMode
                         }}
                     ></script>
-                    <script id="clarity" strategy="afterInteractive">
+                    <script id="clarity">
                         {`(function(c,l,a,r,i,t,y){
                             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
