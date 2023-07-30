8b0d6718090f225bf7b08cf105a4ef04b6d1bdd3
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-07-30 19:37:22 +0300

    Pass children to heading

--- src/components/Mdx/Heading.tsx
+++ src/components/Mdx/Heading.tsx
@@ -1,49 +1,61 @@
 import Link from "next/link";
 
-export const H1 = ({ id, ...rest }) => {
+export const H1 = ({ id, children, ...rest }) => {
     return (
         <Link href={`#${id}`}>
-            <h1 id={id} {...rest} className="anchor" />
+            <h1 id={id} {...rest} className="anchor">
+                {children}
+            </h1>
         </Link>
     );
 };
 
-export const H2 = ({ id, ...rest }) => {
+export const H2 = ({ id, children, ...rest }) => {
     return (
         <Link href={`#${id}`}>
-            <h2 id={id} {...rest} className="anchor" />
+            <h2 id={id} {...rest} className="anchor">
+                {children}
+            </h2>
         </Link>
     );
 };
 
-export const H3 = ({ id, ...rest }) => {
+export const H3 = ({ id, children, ...rest }) => {
     return (
         <Link href={`#${id}`}>
-            <h3 id={id} {...rest} className="anchor" />
+            <h3 id={id} {...rest} className="anchor">
+                {children}
+            </h3>
         </Link>
     );
 };
 
-export const H4 = ({ id, ...rest }) => {
+export const H4 = ({ id, children, ...rest }) => {
     return (
         <Link href={`#${id}`}>
-            <h4 id={id} {...rest} className="anchor" />
+            <h4 id={id} {...rest} className="anchor">
+                {children}
+            </h4>
         </Link>
     );
 };
 
-export const H5 = ({ id, ...rest }) => {
+export const H5 = ({ id, children, ...rest }) => {
     return (
         <Link href={`#${id}`}>
-            <h5 id={id} {...rest} className="anchor" />
+            <h5 id={id} {...rest} className="anchor">
+                {children}
+            </h5>
         </Link>
     );
 };
 
-export const H6 = ({ id, ...rest }) => {
+export const H6 = ({ id, children, ...rest }) => {
     return (
         <Link href={`#${id}`}>
-            <h6 id={id} {...rest} className="anchor" />
+            <h6 id={id} {...rest} className="anchor">
+                {children}
+            </h6>
         </Link>
     );
 };
