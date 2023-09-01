ab13ebcc6c744101e56d5af531bb5a86f6fd224e
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-09-01 12:51:58 +0300

    Comment out webpack config for Preact

--- next.config.js
+++ next.config.js
@@ -83,20 +83,19 @@ const config = {
                 hostname: "cdn.buymeacoffee.com"
             }
         ]
-    },
-    webpack: (config, { dev, isServer }) => {
-        // Replace React with Preact only in client production build
-        if (!dev && !isServer) {
-            Object.assign(config.resolve.alias, {
-                "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-                react: "preact/compat",
-                "react-dom/test-utils": "preact/test-utils",
-                "react-dom": "preact/compat"
-            });
-        }
-
-        return config;
     }
+    // webpack: (config, { dev, isServer }) => {
+    //     // Replace React with Preact only in client production build
+    //     if (!dev && !isServer) {
+    //         Object.assign(config.resolve.alias, {
+    //             "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+    //             react: "preact/compat",
+    //             "react-dom/test-utils": "preact/test-utils",
+    //             "react-dom": "preact/compat"
+    //         });
+    //     }
+    //     return config;
+    // }
 };
 
 export default withPWA(config);
