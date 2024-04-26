41bbae09ef2b9fe7df503c673187c9b17aba9bdc
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-04-26 19:59:25 +0300

    update eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu({
+    stylistic: {
+        indent: 4,
+        semi: true,
+        overrides: {
+            'antfu/top-level-function': 'off',
+            'style/arrow-parens': 'off',
+            'curly': 'off',
+            'style/brace-style': 'off',
+            'antfu/if-newline': 'off',
+        },
+    },
+    typescript: {
+        overrides: {
+            'ts/ban-ts-comment': 'off',
+            'ts/prefer-ts-expect-error': 'off',
+            'ts/consistent-type-definitions': 'off',
+        },
+    },
+    yaml: {
+        overrides: {
+            'yaml/indent': 'off',
+        },
+    },
+    extends: [
+        'next/core-web-vitals',
+        'next',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:styled-components-a11y/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+});
