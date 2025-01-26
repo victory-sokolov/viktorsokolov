473978f2712524ec786118d98c0bfdb93d13310a
Viktor Sokolov <viktorsokolov.and@gmail.com>
2025-01-26 19:29:49 +0200

    ignore ts build errors

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ import runtimeCaching from "next-pwa/cache.js";
 import process from "node:process";
 
 const withBundleAnalyzer = NextBundleAnalyzer({
-    enabled: process.env.ANALYZE === "true"
+    enabled: process.env.ANALYZE === "true",
 });
 
 const withPWA = nextPWA({
@@ -13,7 +13,7 @@ const withPWA = nextPWA({
     runtimeCaching,
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === "development"
+    disable: process.env.NODE_ENV === "development",
 });
 
 const ContentSecurityPolicy = `
@@ -32,36 +32,36 @@ const ContentSecurityPolicy = `
 const securityHeaders = [
     {
         key: "Content-Security-Policy",
-        value: ContentSecurityPolicy.replace(/\n/g, "").trim()
+        value: ContentSecurityPolicy.replace(/\n/g, "").trim(),
     },
     {
         key: "X-Frame-Options",
-        value: "DENY"
+        value: "DENY",
     },
     {
         key: "X-Content-Type-Options",
-        value: "nosniff"
+        value: "nosniff",
     },
     {
         key: "Referrer-Policy",
-        value: "origin-when-cross-origin"
+        value: "origin-when-cross-origin",
     },
     {
         key: "Strict-Transport-Security",
-        value: "max-age=63072000; includeSubDomains; preload"
+        value: "max-age=63072000; includeSubDomains; preload",
     },
     {
         key: "Permissions-Policy",
-        value: "camera=(), geolocation=(), microphone=()"
+        value: "camera=(), geolocation=(), microphone=()",
     },
     {
         key: "X-XSS-Protection",
-        value: "1; mode=block"
+        value: "1; mode=block",
     },
     {
         key: "X-DNS-Prefetch-Control",
-        value: "on"
-    }
+        value: "on",
+    },
 ];
 
 const config = {
@@ -69,19 +69,22 @@ const config = {
         return [
             {
                 source: "/(.*)",
-                headers: securityHeaders
-            }
+                headers: securityHeaders,
+            },
         ];
     },
     poweredByHeader: false,
     experimental: { webVitalsAttribution: ["CLS", "LCP"] },
     compiler: {
-        styledComponents: true
+        styledComponents: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
     },
     modularizeImports: {
         "react-icons": {
-            transform: "react-icons/{{member}}"
-        }
+            transform: "react-icons/{{member}}",
+        },
     },
     images: {
         formats: ["image/webp", "image/avif"],
@@ -89,14 +92,14 @@ const config = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "cdn.buymeacoffee.com"
-            }
-        ]
+                hostname: "cdn.buymeacoffee.com",
+            },
+        ],
     },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
-        ignoreDuringBuilds: true
+        ignoreDuringBuilds: true,
     },
     transpilePackages: ["next-mdx-remote"],
     async redirects() {
@@ -104,20 +107,20 @@ const config = {
             {
                 source: "/index",
                 destination: "/",
-                permanent: true
+                permanent: true,
             },
             {
                 source: "/tip/:slug",
                 destination: "/tips/:slug",
-                permanent: true
+                permanent: true,
             },
             {
                 source: "/post/:slug",
                 destination: "/blog/:slug",
-                permanent: true
-            }
+                permanent: true,
+            },
         ];
-    }
+    },
 };
 
 export default withBundleAnalyzer(withPWA(config));
