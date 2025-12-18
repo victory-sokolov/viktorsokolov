bc4462b58db69511658d6f2645d7eb1dcdf1edb0
Viktor Sokolov <viktorsokolov.and@gmail.com>
2025-12-18 21:31:53 +0200

    fix center layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,9 +2,9 @@
 
 import type { ReactProps } from "src/types/types";
 
-import React from "react";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import React from "react";
 
 
 
@@ -13,7 +13,7 @@ const Layout: React.FC<ReactProps> = ({ children }) => {
 
     return (
         <div className="min-h-screen">
-            <div className="max-w-screen-sm mx-auto px-16 max-sm:px-8">
+            <div className="w-full max-w-[850px] mx-auto! px-10! max-sm:px-8!">
                 <Header />
                 <div className="mt-24 pb-20 max-sm:mt-16">{children}</div>
                 <Footer />
