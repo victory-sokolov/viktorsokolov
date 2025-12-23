bea5fb27b3a841b403cde4cb52d8a7bcf5526895
Viktor Sokolov <viktorsokolov.and@gmail.com>
2025-12-23 22:23:08 +0200

    fix types

--- src/components/Mdx/MDXComponent.tsx
+++ src/components/Mdx/MDXComponent.tsx
@@ -10,7 +10,7 @@ import { H1, H2, H3, H4, H5, H6 } from "./Heading";
 import { TechStackList } from "./TechStackList";
 
 type MDXComponentsProp = {
-    [key: string]: ComponentType<unknown>;
+    [key: string]: React.FC<any>;
 };
 
 const CustomLink = ({ href, children }) => {
--- src/components/Mdx/MDXRemote.tsx
+++ src/components/Mdx/MDXRemote.tsx
@@ -5,5 +5,5 @@ import { MDXRemote } from "next-mdx-remote";
 import { MDXComponents } from "@/components/Mdx";
 
 export const MdxRemote = ({ source }: { source: MDXRemoteSerializeResult }) => {
-    return <MDXRemote {...source} components={MDXComponents} />;
+    return <MDXRemote {...source} components={MDXComponents as any} />;
 };
