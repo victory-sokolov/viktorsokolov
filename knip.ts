8f53ebb91c9f6fb4b4a05e832542d96a1c847f51
Viktor Sokolov <viktorsokolov.and@gmail.com>
2025-12-23 19:39:44 +0200

    update knip

--- knip.ts
+++ knip.ts
@@ -2,4 +2,7 @@ export default {
     compilers: {
         mdx: true,
     },
+    ignore: [".lighthouserc.js", "**/*.mdx"],
+    // Dependencies to ignore (not reported as unused)
+    ignoreDependencies: ["sharp"],
 };
