5930ff37fb0c9f347b8d9ac1e1722d27f816b398
Viktor Sokolov <viktorsokolov.and@gmail.com>
2025-12-23 23:52:23 +0200

    update types

--- src/components/Mdx/Heading.tsx
+++ src/components/Mdx/Heading.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 const createHeading = (level: number) => ({ id, children, ...rest }) => {
-    const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+    const Tag = `h${level}` as "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
     return (
         <Link href={`#${id}`}>
             <Tag id={id} {...rest} className="anchor">
